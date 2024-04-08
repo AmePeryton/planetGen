aa3f74de88e8a3433016272e8d7b1e26718cb9f3
AmePeryton <ampric15@asu.edu>
2024-04-07 17:21:56 -0800

    Blur Effect
    + Imported Kawase Blue package for blurred UI backgrounds using URP
    ~ Changed pause menu to blur background
    + Started stellar system UI prefab creation

---
+++ Assets/Gameplay Beta/V1_StellarSystem/V1_StellarSystemUI.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class V1_StellarSystemUI : MonoBehaviour
+{
+	void Start()
+	{
+		
+	}
+
+	void Update()
+	{
+		
+	}
+}
